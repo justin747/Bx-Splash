b53942e27da7cd8ce249772328340d4cb88e2c95
justin747 <justin747@gmail.com>
2022-07-24 21:42:42 -0700

    Initial Commit

---
+++ Bx Splash/Bx_SplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bx_SplashApp.swift
+//  Bx Splash
+//
+//  Created by Justin on 7/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Bx_SplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
